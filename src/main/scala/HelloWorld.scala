192dec74aa34e0e51fe6e044a5eef30b611b7026
ChanderG <chandergovind@gmail.com>
2015-02-26 15:00:08 +0530

    adds base

--- src/main/scala/HelloWorld.scala
+++ src/main/scala/HelloWorld.scala
@@ -1,3 +1,16 @@
+import scala.io.Source
+
 object Main extends App { 
-  Console.println("Hello World!") 
+
+  def Usuage {
+    println("Usuage: \nEnter the name of the file to train upon.")
+  }
+  
+  override def main(args: Array[String]){
+    //considering only the first parameter of input
+
+    val lines = Source.fromFile(args.head).mkString
+    val input = lines.split("\n").mkString
+    println(input)
+  }
 }
